e30e08a07e78d694271472263251178eb129060b
jaba.kochashvili.1 <jaba.kochashvili.1@btu.edu.ge>
2026-03-14 12:58:14 +0000

    setup env

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,10 +6,11 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
+    pkgs.python310
+    pkgs.python310Packages.pip
+    pkgs.poetry
   ];
   # Sets environment variables in the workspace
   env = {};
